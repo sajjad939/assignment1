c706161e4baad60da0667bf57ddb7026e557f74a
sajjad939 <140053980+sajjad939@users.noreply.github.com>
2025-04-29 17:14:17 +0500

    CampusParkingManagementSystem.java

--- CampusParkingManagementSystem.java
+++ CampusParkingManagementSystem.java
@@ -1,4 +1,3 @@
-import java.util.*;
 
 abstract class Person {
     private String name;
@@ -21,13 +20,9 @@ class Supervisor extends Person {
         this.yearsOfExperience = years;
     }
 
-    public int getYearsOfExperience() {
-         return yearsOfExperience; 
-        }
+    public int getYearsOfExperience() { return yearsOfExperience; }
 
-    public void setYearsOfExperience(int years) {
-         this.yearsOfExperience = years;
-         }
+    public void setYearsOfExperience(int years) { this.yearsOfExperience = years; }
 }
 
 class Owner extends Person {
@@ -62,8 +57,6 @@ class PermitHolder extends Person {
         return permitId.hashCode();
     }
 }
-
-
 class Vehicle implements Cloneable {
     private String licensePlate;
     private String type;
@@ -75,86 +68,89 @@ class Vehicle implements Cloneable {
         this.owner = owner;
     }
 
-    public String getLicensePlate() { return licensePlate; }
+    public String getLicensePlate() { 
+        return licensePlate; 
+    }
 
-    public String getType() { return type; }
+    public String getType() { 
+        return type;
+    }
 
-    public Owner getOwner() { return owner; }
+    public Owner getOwner() {
+        return owner; 
+    }
 
-    public void setOwner(Owner owner) { this.owner = owner; }
-
- 
+    public void setOwner(Owner owner) {
+        this.owner = owner; 
+    }
     public Vehicle shallowClone() {
-        try {
-            return (Vehicle) super.clone();
-        } catch (CloneNotSupportedException e) {
-            return null;
-        }
-    }
+    return new Vehicle(this.licensePlate, this.type, this.owner); 
+}
 
-   
-    public Vehicle deepClone() {
-        return new Vehicle(this.licensePlate, this.type, new Owner(this.owner.getName()));
     }
+    public Vehicle deepClone() {
+    String newLicense = this.licensePlate;
+    String newType = this.type;
+    String ownerName = this.owner.getName();
+    Owner newOwner = new Owner(ownerName);
+    return new Vehicle(newLicense, newType, newOwner);
+}
+
 
     @Override
     public String toString() {
         return licensePlate + " (" + type + "), Owner: " + owner.getName();
     }
 }
-
-
 class ParkingZone {
-    private static int zoneCounter = 1;
+    private static int counter = 1;
     private final String zoneId;
     private Vehicle[] vehicles = new Vehicle[5];
+    private int vehicleCount = 0;
 
     public ParkingZone() {
-        this.zoneId = "A" + zoneCounter++;
+        this.zoneId = "A" + counter++;
     }
 
     public String getZoneId() { return zoneId; }
 
     public boolean addVehicle(Vehicle v) {
-        for (Vehicle vehicle : vehicles) {
-            if (vehicle != null && vehicle.getLicensePlate().equals(v.getLicensePlate())) {
+        for (int i = 0; i < vehicleCount; i++) {
+            if (vehicles[i].getLicensePlate().equals(v.getLicensePlate())) {
                 System.out.println("Vehicle with same license already exists.");
                 return false;
             }
         }
-        for (int i = 0; i < vehicles.length; i++) {
-            if (vehicles[i] == null) {
-                vehicles[i] = v;
-                return true;
-            }
+        if (vehicleCount < vehicles.length) {
+            vehicles[vehicleCount++] = v;
+            return true;
+        } else {
+            System.out.println("Zone full.");
+            return false;
         }
-        System.out.println("Zone full.");
-        return false;
     }
 
     public void displayVehicles() {
         System.out.println("Zone " + zoneId + ":");
-        for (Vehicle v : vehicles) {
-            if (v != null) System.out.println("  - " + v);
+        for (int i = 0; i < vehicleCount; i++) {
+            System.out.println("  - " + vehicles[i]);
         }
     }
 }
-
-
 class ParkingSystem {
     private static ParkingSystem instance;
     private String campusName;
     private Supervisor supervisor;
-    private List<ParkingZone> zones = new ArrayList<>();
-    private List<PermitHolder> permitHolders = new ArrayList<>();
+    private ParkingZone[] zones = new ParkingZone[5];
+    private PermitHolder[] permitHolders = new PermitHolder[10];
+    private int zoneCount = 0;
+    private int permitCount = 0;
 
-    // Private constructor
     private ParkingSystem(String campusName, Supervisor supervisor) {
         this.campusName = campusName;
         this.supervisor = supervisor;
     }
 
-   
     public static ParkingSystem getInstance(String campusName, Supervisor supervisor) {
         if (instance == null) {
             instance = new ParkingSystem(campusName, supervisor);
@@ -163,60 +159,64 @@ class ParkingSystem {
     }
 
     public void addZone(ParkingZone zone) {
-        zones.add(zone);
+        if (zoneCount < zones.length) {
+            zones[zoneCount++] = zone;
+        }
     }
 
     public void addPermitHolder(PermitHolder ph) {
-        permitHolders.add(ph);
+        if (permitCount < permitHolders.length) {
+            permitHolders[permitCount++] = ph;
+        }
     }
 
     public void displaySystemInfo() {
         System.out.println("Campus: " + campusName);
-        System.out.println("Supervisor: " + supervisor.getName() + " (" + supervisor.getYearsOfExperience() + " years experience)");
+        System.out.println("Supervisor: " + supervisor.getName() + "" + supervisor.getYearsOfExperience() + " years experience");
         System.out.println("Parking Zones:");
-        for (ParkingZone zone : zones) {
-            zone.displayVehicles();
+        for (int i = 0; i < zoneCount; i++) {
+            zones[i].displayVehicles();
         }
         System.out.println("Permit Holders:");
-        for (PermitHolder ph : permitHolders) {
-            System.out.println("  - " + ph.getName() + " (ID: " + ph.getPermitId() + ")");
+        for (int i = 0; i < permitCount; i++) {
+            System.out.println("  - " + permitHolders[i].getName() + " ID: " + permitHolders[i].getPermitId() + "");
         }
     }
 }
 public class CampusParkingManagementSystem {
     public static void main(String[] args) {
-      
         Supervisor supervisor = new Supervisor("Dr. Alice", 15);
         ParkingSystem system = ParkingSystem.getInstance("Greenfield Campus", supervisor);
 
-        
         ParkingZone zone1 = new ParkingZone();
         ParkingZone zone2 = new ParkingZone();
         system.addZone(zone1);
         system.addZone(zone2);
 
-        
         PermitHolder ph1 = new PermitHolder("John Doe");
         PermitHolder ph2 = new PermitHolder("Jane Smith");
         system.addPermitHolder(ph1);
         system.addPermitHolder(ph2);
+
         Owner owner1 = new Owner("Mark");
         Owner owner2 = new Owner("Lisa");
 
         Vehicle v1 = new Vehicle("ABC123", "Car", owner1);
         Vehicle v2 = new Vehicle("XYZ789", "Bike", owner2);
 
-        
         zone1.addVehicle(v1);
         zone1.addVehicle(v2);
+
         Vehicle shallowCopy = v1.shallowClone();
         Vehicle deepCopy = v1.deepClone();
+
         owner1.setName("Changed Mark");
 
         System.out.println("\n--- Clone Demonstration ---");
         System.out.println("Original: " + v1);
         System.out.println("Shallow Copy: " + shallowCopy);
         System.out.println("Deep Copy: " + deepCopy);
+
         System.out.println("\n--- System Info ---");
         system.displaySystemInfo();
     }
